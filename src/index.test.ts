8e46c492c3cd7b6c40cb92ddecd6fa00a8fc834b
Matthew Rose <matt.usurp@gmail.com>
2022-04-10 20:58:33 +0100

    chore: cleanup tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,15 +2,13 @@ import { never } from './core/assert-never';
 import { okey, okeys, okv, okvr } from './core/object';
 import * as index from './index';
 
-describe('index', (): void => {
-  describe('exports', (): void => {
-    // core/assert-never
-    it('exports never', () => expect(index.never).toStrictEqual(never));
+describe('exports', (): void => {
+  // core/assert-never
+  it('exports never', () => expect(index.never).toStrictEqual(never));
 
-    // core/object
-    it('exports okey', () => expect(index.okey).toStrictEqual(okey));
-    it('exports okeys', () => expect(index.okeys).toStrictEqual(okeys));
-    it('exports okv', () => expect(index.okv).toStrictEqual(okv));
-    it('exports okvr', () => expect(index.okvr).toStrictEqual(okvr));
-  });
-})
+  // core/object
+  it('exports okey', () => expect(index.okey).toStrictEqual(okey));
+  it('exports okeys', () => expect(index.okeys).toStrictEqual(okeys));
+  it('exports okv', () => expect(index.okv).toStrictEqual(okv));
+  it('exports okvr', () => expect(index.okvr).toStrictEqual(okvr));
+});
