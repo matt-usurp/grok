70daca7d6a29f5143c5925a1569e9502d65cca57
Matthew Rose <matt.usurp@gmail.com>
2022-09-14 18:57:22 +0800

    feat(inherit): introduce `isInherit` for checking `inherit` symbol

--- src/core/inherit.ts
+++ src/core/inherit.ts
@@ -1,4 +1,13 @@
+import { Grok } from '../index';
+
 /**
  * A symbol that is used for uniquly identifying an inherit action.
  */
 export const inherit = Symbol('grok:inherit');
+
+/**
+ * Check the given {@link value} is the {@link inherit} symbol.
+ */
+export const isInherit = (value: unknown): value is Grok.Inherit => {
+  return value === inherit;
+};
